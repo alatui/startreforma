f02a9d82a703667d92fcd232c0365e63c3fdcf72
Fernando Cisneros <fernando.saxofone@gmail.com>
2017-10-02 19:38:37 +0100

    fixed typos index

--- js/custom.js
+++ js/custom.js
@@ -142,7 +142,7 @@ $(document).ready(function () {
         navigation: false, // Show next and prev buttons
         slideSpeed: 800,
         paginationSpeed: 400,
-        autoPlay: 5000,
+        autoPlay: 10000,
         singleItem: true
     });
 
